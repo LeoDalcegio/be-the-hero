c84c116806dfd2ec10cbc58dd849855a0e54f546
Leonardo Luís Dalcegio <leoodalcegio@hotmail.com>
2020-03-31 07:55:56 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -31,16 +31,16 @@ routes.post('/incidents', celebrate({
         authorization: Joi.string().required()
         ,}).unknown(),
 
-        [Segments.BODY]: Joi.object().keys({
-            name: Joi.string().required(),
-            email: Joi.string().required().email(),
-            whatsapp: Joi.string().required().min(10).max(11),
-            city: Joi.string().required(),
-            uf: Joi.string().required().length(2),
-            title: Joi.string().required(),
-            description: Joi.string().required(),
-            value: Joi.number().required()
-        }),
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required()
+    }),
 }),IncidentController.create);
 
 routes.get('/incidents', celebrate({
